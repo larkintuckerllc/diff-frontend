c9565a6c7b83d069fc42ce3ed22e9bbc9a2682b6
John Tucker <jtucker@appirio.com>
2019-10-13 08:20:12 -0400

    refetch

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,14 +1,19 @@
 import { useQuery } from '@apollo/react-hooks';
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { BooksData, BOOKS } from '../graphql/books';
 
 const Books: FC = () => {
-  const { loading, error, data } = useQuery<BooksData>(BOOKS);
-
+  const { loading, error, data, refetch } = useQuery<BooksData>(BOOKS);
+  const handleClick = useCallback(() => {
+    refetch();
+  }, [refetch]);
   if (loading) return <p>Loading...</p>;
   if (error || data === undefined) return <p>Error :(</p>;
   return (
     <div>
+      <button onClick={handleClick} type="button">
+        Refetch
+      </button>
       {data.books.map(({ author, id, title }) => (
         <div key={id} style={{ marginTop: 30 }}>
           <div>
