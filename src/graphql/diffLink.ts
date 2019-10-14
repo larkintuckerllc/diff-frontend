cc39234cbcb4fea45c165f579a15024f7ddf2c0a
John Tucker <jtucker@appirio.com>
2019-10-14 19:46:04 -0400

    Done

--- src/graphql/diffLink.ts
+++ src/graphql/diffLink.ts
@@ -1,74 +1,82 @@
+import { ApolloCache } from 'apollo-cache';
 import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-link';
-import { BOOKS, BOOKS_UPDATE, BooksData, BooksUpdateData } from './books';
+import { ApolloLink, Operation } from 'apollo-link';
+import { Book, BOOKS, BOOKS_UPDATE, BooksData, BooksUpdateData } from './books';
 import { booksGetLastModified, booksSetLastModified } from './lastModified';
 
-export default new ApolloLink((operation, forward) => {
-  const context = operation.getContext();
-  const { cache } = context as ApolloClient<object>;
-  const booksLastModified = booksGetLastModified();
-  // TODO: MAKE MORE PURE
+// eslint-disable-next-line
+type Data = { [key: string]: any };
+
+const mutateOperation = (operation: Operation): void => {
   const mutatedOperation = operation;
   const { operationName } = operation;
   switch (operationName) {
-    case 'books':
+    case 'books': {
+      const booksLastModified = booksGetLastModified();
       mutatedOperation.operationName = 'booksUpdate';
       mutatedOperation.query = BOOKS_UPDATE;
       mutatedOperation.variables = {
         lastModified: booksLastModified,
       };
       break;
+    }
     default:
   }
-  return forward(mutatedOperation).map(result => {
+};
+
+const transformedData = (operationName: string, cache: ApolloCache<object>, data: Data): Data => {
+  switch (operationName) {
+    case 'books': {
+      const { booksUpdate } = data as BooksUpdateData;
+      const booksLastModified = booksGetLastModified();
+      booksSetLastModified(1);
+      // FIRST LOAD
+      if (booksLastModified === 0) {
+        return {
+          books: booksUpdate.filter(({ isDeleted }) => !isDeleted),
+        };
+      }
+      // SUBSEQUENT LOADS
+      const booksCacheData = cache.readQuery<BooksData>({ query: BOOKS });
+      if (booksCacheData === null) {
+        throw new Error(); // UNEXPECTED
+      }
+      const mutatedBooks = [...booksCacheData.books];
+      booksUpdate.forEach(bookUpdate => {
+        const bookMutatedIndex = mutatedBooks.findIndex(book => book.id === bookUpdate.id);
+        if (bookMutatedIndex === -1 && !bookUpdate.isDeleted) {
+          // CASE CREATE
+          mutatedBooks.push(bookUpdate);
+        } else if (bookMutatedIndex !== -1 && bookUpdate.isDeleted) {
+          // CASE DELETE
+          mutatedBooks.splice(bookMutatedIndex, 1);
+        } else if (bookMutatedIndex !== -1 && !bookUpdate.isDeleted) {
+          // CASE UPDATE
+          mutatedBooks.splice(bookMutatedIndex, 1);
+          mutatedBooks.push(bookUpdate);
+        }
+      });
+      return {
+        books: mutatedBooks,
+      };
+    }
+    default:
+  }
+  return data;
+};
+
+export default new ApolloLink((operation, forward) => {
+  const { operationName } = operation;
+  const context = operation.getContext();
+  const { cache } = context as ApolloClient<object>;
+  mutateOperation(operation); // NOT PURE
+  return forward(operation).map(result => {
     const { data } = result;
     if (data === undefined || data === null) {
       return result;
     }
-    let mutatedData = data;
-    switch (operationName) {
-      case 'books': {
-        const booksUpdateData = data as BooksUpdateData;
-        if (booksLastModified === 0) {
-          mutatedData = {
-            books: booksUpdateData.booksUpdate.filter(({ isDeleted }) => !isDeleted),
-          };
-        } else {
-          const booksCacheData = cache.readQuery<BooksData>({ query: BOOKS });
-          if (booksCacheData === null) {
-            throw new Error(); // UNEXPECTED
-          }
-          // TODO: REFACTOR TO BE MORE PURE
-          const mutatedBooks = [...booksCacheData.books];
-          booksUpdateData.booksUpdate.forEach(bookUpdate => {
-            const bookMutatedIndex = mutatedBooks.findIndex(book => book.id === bookUpdate.id);
-            if (bookMutatedIndex === -1 && !bookUpdate.isDeleted) {
-              // CASE CREATE
-              const createBook = { ...bookUpdate };
-              delete createBook.isDeleted;
-              mutatedBooks.push(createBook);
-            } else if (bookMutatedIndex !== -1 && bookUpdate.isDeleted) {
-              // CASE DELETE
-              mutatedBooks.splice(bookMutatedIndex, 1);
-            } else if (bookMutatedIndex !== -1 && !bookUpdate.isDeleted) {
-              // CASE UPDATE
-              mutatedBooks.splice(bookMutatedIndex, 1);
-              const createBook = { ...bookUpdate };
-              delete createBook.isDeleted;
-              mutatedBooks.push(createBook);
-            }
-          });
-          mutatedData = {
-            books: mutatedBooks,
-          };
-        }
-        booksSetLastModified(1);
-        break;
-      }
-      default:
-    }
     return {
-      data: mutatedData,
+      data: transformedData(operationName, cache, data),
     };
   });
 });
